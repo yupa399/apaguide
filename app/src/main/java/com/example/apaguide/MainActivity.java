1cbbd9193c6f487d63b8513157ed0ce7dfe5dabc
pyunqi <pyunqi@gmail.com>
2019-11-14 20:59:24 +1300

    extend slide area

--- app/src/main/java/com/example/apaguide/MainActivity.java
+++ app/src/main/java/com/example/apaguide/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.apaguide;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.example.apaguide.model.Example;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.DisplayMetrics;
 import android.view.View;
 
+import androidx.customview.widget.ViewDragHelper;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,6 +24,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.WindowManager;
 
 import java.io.IOException;
 
@@ -36,17 +40,49 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        setLeftMargin(drawer,0.5f);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_avoid_plagiarism,R.id.nav_apa_important,R.id.nav_home, R.id.nav_guide, R.id.nav_examples,
+                R.id.nav_avoid_plagiarism, R.id.nav_apa_important, R.id.nav_home, R.id.nav_guide, R.id.nav_examples,
                 R.id.nav_word, R.id.nav_material, R.id.nav_faq, R.id.nav_group_credit)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    /**
+     * extend slide area
+     * @param drawer
+     * @param percent
+     */
+    private void setLeftMargin(DrawerLayout drawer, float percent) {
+        java.lang.reflect.Field mDragger = null;
+        try {
+            mDragger = drawer.getClass().getDeclaredField("mLeftDragger");
+            mDragger.setAccessible(true);
+            ViewDragHelper draggerObj = null;
+            draggerObj = (ViewDragHelper) mDragger.get(drawer);
+
+            java.lang.reflect.Field mEdgeSize = null;
+            mEdgeSize = draggerObj.getClass().getDeclaredField("mEdgeSize");
+            mEdgeSize.setAccessible(true);
+            int edgeSize = mEdgeSize.getInt(draggerObj);
+
+            WindowManager manager = this.getWindowManager();
+            DisplayMetrics outMetrics = new DisplayMetrics();
+            manager.getDefaultDisplay().getMetrics(outMetrics);
+
+            mEdgeSize.setInt(draggerObj, Math.max(edgeSize, (int) (outMetrics.widthPixels * percent)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
